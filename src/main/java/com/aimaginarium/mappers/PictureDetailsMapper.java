1092b5ca0e2a905383aede729a04a50d273c48e4
Artem_Kovalov <artemkov1990@gmail.com>
2024-04-08 22:57:24 +0300

    added Mapstruct mapper

---
+++ src/main/java/com/aimaginarium/mappers/PictureDetailsMapper.java
@@ -0,0 +1,11 @@
+package com.aimaginarium.mappers;
+
+import com.aimaginarium.dtos.PictureDetailsDto;
+import com.aimaginarium.models.PictureDetails;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PictureDetailsMapper extends Mappable<PictureDetails, PictureDetailsDto> {
+}
