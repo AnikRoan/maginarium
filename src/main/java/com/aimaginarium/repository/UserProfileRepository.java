822302f9172ba040de02a29c6572d9e00c3560ba
Anna Padun <roananik1988@gmail.com>
2024-04-09 11:06:56 +0200

    naming

--- src/main/java/com/aimaginarium/repository/UserProfileRepository.java
+++ src/main/java/com/aimaginarium/repository/UserProfileRepository.java
@@ -1,6 +1,6 @@
 package com.aimaginarium.repository;
 
-import com.aimaginarium.models.UserProfile;
+import com.aimaginarium.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
