14a559bbac537fb8c9cc0ff74931006409818e17
Artem_Kovalov <artemkov1990@gmail.com>
2024-04-16 14:08:34 +0300

    added mappers for new DTOs

---
+++ src/main/java/com/aimaginarium/mapper/UserGalleryUpdateMapper.java
@@ -0,0 +1,11 @@
+package com.aimaginarium.mapper;
+
+import com.aimaginarium.dto.UserGalleryUpdateDto;
+import com.aimaginarium.model.UserGallery;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserGalleryUpdateMapper extends Mappable<UserGallery, UserGalleryUpdateDto> {
+}
