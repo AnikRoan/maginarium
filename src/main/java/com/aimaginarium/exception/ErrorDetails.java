689a60cd721e489b580582cd82cdf280e25d20a3
Artem <105941754+ArtemJT@users.noreply.github.com>
2024-04-20 23:05:00 +0300

    changed ErrorDetails and ErrorMessage into Record

--- src/main/java/com/aimaginarium/exception/ErrorDetails.java
+++ src/main/java/com/aimaginarium/exception/ErrorDetails.java
@@ -1,24 +1,21 @@
 package com.aimaginarium.exception;
 
 import lombok.Builder;
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
-@Data
 @Builder
-public class ErrorDetails {
+public record ErrorDetails(
+        LocalDateTime timestamp,
+        String path,
+        Map<String, String> errors) {
 
-    private LocalDateTime timestamp;
-    private String path;
-    private Map<String, String> errors;
-
-    public static ResponseEntity<ErrorDetails> getResponseEntityErrorsMap(String path,
-                                                                          HttpStatus status,
-                                                                          Map<String, String> errors) {
+    public static ResponseEntity<ErrorDetails> getResponseEntityErrorsMap(final String path,
+                                                                          final HttpStatus status,
+                                                                          final Map<String, String> errors) {
         ErrorDetails errorDetails = ErrorDetails.builder()
                 .timestamp(LocalDateTime.now())
                 .path(path)
--- src/main/java/com/aimaginarium/exception/ErrorMessage.java
+++ src/main/java/com/aimaginarium/exception/ErrorMessage.java
@@ -1,8 +1,11 @@
 package com.aimaginarium.exception;
 
 
-public class ErrorMessage {
+public record ErrorMessage() {
     public static final String USER_NOT_FOUND = "User [%s] not found";
     public static final String USER_DETAILS_NOT_FOUND = "User details by user [%s] not found";
     public static final String INVALID_PASSWORD = "Wrong password";
+    public static final String GALLERY_NOT_FOUND_BY_USER = "Gallery with user id=[%s] not found";
+    public static final String GALLERY_NOT_FOUND = "Gallery with id=[%s] not found";
+    public static final String GALLERY_ALREADY_EXIST = "Gallery already exist by user id=[%s]";
 }
